c6d3f38925e7813ae54f14257f3004c60c59db4b
nvggupta <ng969028@gmail.com>
2024-12-18 13:06:00 +0530

    final commit

--- src/Components/Admin/Vendor.jsx
+++ src/Components/Admin/Vendor.jsx
@@ -9,6 +9,7 @@ function Vendor() {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [approveLoading, setAppproveLoading] = useState({});
+  const [searchTerm, setSearchTerm] = useState("");
   const itemsPerPage = 10;
 
   const getVendorList = async () => {
@@ -38,20 +39,33 @@ function Vendor() {
     console.log("Hello");
     console.log("Vendor List", vendor);
   }, []);
+
+  const filteredVendors = vendor.filter((vendorItem) =>
+    vendorItem.name?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    vendorItem.email?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    vendorItem.mobile?.toString().includes(searchTerm)
+  );
+
   const last = currentPage * itemsPerPage;
   const first = last - itemsPerPage;
-  const currentItems = vendor.slice(first, last);
+  const currentItems = filteredVendors.slice(first, last);
 
-  const totalPages = Math.ceil(vendor.length / itemsPerPage);
+  const totalPages = Math.ceil(filteredVendors.length / itemsPerPage);
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    setCurrentPage(1);
+  };
+
   const handleApproveVendor = async (id) => {
     console.log(id);
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
     try {
-      setAppproveLoading(prev => ({...prev, [id]: true}));
+      setAppproveLoading((prev) => ({ ...prev, [id]: true }));
       const response = await axios.post(
         "https://rfpdemo.velsof.com/api/approveVendor",
         {
@@ -65,115 +79,131 @@ function Vendor() {
           },
         }
       );
-      console.log(response.data)
+      console.log(response.data);
       if (response.data.response === "success") {
-
-      toast.success(response.data.response);
-      setAppproveLoading(prev => ({...prev, [id]: false}));
-      getVendorList();
+        toast.success(response.data.response);
+        setAppproveLoading((prev) => ({ ...prev, [id]: false }));
+        getVendorList();
+      } else {
+        typeof error === "object"
+          ? toast.error(response.data.errors[0])
+          : toast.error(
+              response.data.errors ||
+                response.data.message ||
+                response.data.error
+            );
+        setAppproveLoading((prev) => ({ ...prev, [id]: false }));
       }
-      else {
-        typeof error === "object" ? toast.error(response.data.errors[0]) : toast.error(response.data.errors || response.data.message || response.data.error);
-        setAppproveLoading(prev => ({...prev, [id]: false}));
-      }
-    
     } catch (error) {
       toast.error("Error approving vendor");
       console.log(error);
-      setAppproveLoading(prev => ({...prev, [id]: false}));
+      setAppproveLoading((prev) => ({ ...prev, [id]: false }));
     }
   };
   return (
-      <div className="p-4 ">
-        <ToastContainer />
-        <h1 className="text-2xl font-bold mb-4">Vendor List</h1>
-        {loading ? (
-          <p className="text-blue-500">Loading...</p>
-        ) : vendor.length > 0 ? (
-          <>
-            <table className="w-full border-collapse border border-gray-300 mb-4">
-              <thead>
-                <tr className="bg-gray-100">
-                  <th className="border border-gray-300 p-2">ID</th>
-                  <th className="border border-gray-300 p-2">First-Name</th>
-                  <th className="border border-gray-300 p-2">Last-Name</th>
-                  <th className="border border-gray-300 p-2">Email</th>
-                  <th className="border border-gray-300 p-2">Contact-No</th>
-                  <th className="border border-gray-300 p-2">vendor-Status</th>
-                  <th className="border border-gray-300 p-2">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentItems.map((vendorItem) => (
-                  <tr
-                    key={vendorItem.id}
-                    className="text-center hover:bg-gray-100"
-                  >
-                    <td className="border border-gray-300 p-2">
-                      {vendorItem.user_id}
-                    </td>
-                    <td className="border border-gray-300 p-2">
-                      {vendorItem.name.split(" ")[0]}
-                    </td>
-                    <td className="border border-gray-300 p-2">
-                      {vendorItem.name.split(" ")[1]}
-                    </td>
-                    <td className="border border-gray-300 p-2">
-                      {vendorItem.email}
-                    </td>
-                    <td className="border border-gray-300 p-2">
-                      {vendorItem.mobile}
-                    </td>
-                    {
-                      <td
-                        className={`border border-gray-300 p-2 ${
-                          vendorItem.status === "Approved"
-                            ? "text-green-500"
-                            : "text-red-500"
-                        }`}
-                      >
-                        {vendorItem.status}
-                      </td>
-                    }
+    <div className="p-4 ">
+      <ToastContainer />
+      <div className="flex justify-between pr-5">
+
+      <h1 className="text-2xl font-bold mb-4">Vendor List</h1>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search vendors..."
+          value={searchTerm}
+          onChange={handleSearch}
+          className="p-2 border border-gray-300 rounded w-full max-w-md"
+        />
+      </div>
+      </div>
+      {loading ? (
+        <p className="text-blue-500">Loading...</p>
+      ) : filteredVendors.length > 0 ? (
+        <>
+          <table className="w-full border-collapse border border-gray-300 mb-4">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="border border-gray-300 p-2">ID</th>
+                <th className="border border-gray-300 p-2">First-Name</th>
+                <th className="border border-gray-300 p-2">Last-Name</th>
+                <th className="border border-gray-300 p-2">Email</th>
+                <th className="border border-gray-300 p-2">Contact-No</th>
+                <th className="border border-gray-300 p-2">vendor-Status</th>
+                <th className="border border-gray-300 p-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentItems.map((vendorItem) => (
+                <tr
+                  key={vendorItem.id}
+                  className="text-center hover:bg-gray-100"
+                >
+                  <td className="border border-gray-300 p-2">
+                    {vendorItem.user_id}
+                  </td>
+                  <td className="border border-gray-300 p-2">
+                    {vendorItem.name.split(" ")[0]}
+                  </td>
+                  <td className="border border-gray-300 p-2">
+                    {vendorItem.name.split(" ")[1]}
+                  </td>
+                  <td className="border border-gray-300 p-2">
+                    {vendorItem.email}
+                  </td>
+                  <td className="border border-gray-300 p-2">
+                    {vendorItem.mobile}
+                  </td>
+                  {
                     <td
-                      className={`border border-gray-300 text-red-500 p-2 cursor-pointer ${
+                      className={`border border-gray-300 p-2 ${
                         vendorItem.status === "Approved"
-                          ? "cursor-not-allowed"
-                          : ""
+                          ? "text-green-500"
+                          : "text-red-500"
                       }`}
-                      onClick={() => handleApproveVendor(vendorItem.user_id)}
                     >
-                      {vendorItem.status !== "Approved"
+                      {vendorItem.status}
+                    </td>
+                  }
+                  <td
+                    className={`border border-gray-300 text-red-500 p-2 cursor-pointer ${
+                      vendorItem.status === "Approved"
+                        ? "cursor-not-allowed"
+                        : ""
+                    }`}
+                    onClick={() => handleApproveVendor(vendorItem.user_id)}
+                  >
+                    {vendorItem.status !== "Approved"
                       ? approveLoading[vendorItem.user_id]
                         ? "Loading..."
                         : "Approve"
                       : ""}
-                    </td>
-                  </tr>                ))}
-              </tbody>
-            </table>
-            <div className="flex justify-center items-center gap-2">
-              {Array.from({ length: totalPages }, (_, index) => index + 1).map(
-                (pageNumber) => (
-                  <button
-                    key={pageNumber}
-                    onClick={() => handlePageChange(pageNumber)}
-                    className={`px-3 py-1 border rounded ${
-                      pageNumber === currentPage
-                        ? "bg-blue-500 text-white"
-                        : "bg-white text-blue-500 border-blue-500"
-                    }`}
-                  >
-                    {pageNumber}
-                  </button>
-                )
-              )}
-            </div>
-          </>
-        ) : (
-          <p className="text-red-500">No vendors found.</p>
-        )}
-      </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="flex justify-center items-center gap-2">
+            {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+              (pageNumber) => (
+                <button
+                  key={pageNumber}
+                  onClick={() => handlePageChange(pageNumber)}
+                  className={`px-3 py-1 border rounded ${
+                    pageNumber === currentPage
+                      ? "bg-blue-500 text-white"
+                      : "bg-white text-blue-500 border-blue-500"
+                  }`}
+                >
+                  {pageNumber}
+                </button>
+              )
+            )}
+          </div>
+        </>
+      ) : (
+        <p className="text-red-500">No vendors found.</p>
+      )}
+    </div>
   );
 }
 
